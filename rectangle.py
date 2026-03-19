fb741c0ed77070959aa10912839e10fdf33a718c
Easy Eight <Victor.hoegy@protonmail.com>
2026-03-19 11:25:37 +0100

    Examples of the Rectangle class

---
+++ rectangle.py
@@ -0,0 +1,19 @@
+# Exemple : Declaration of a class with attributes and methods
+class RectangleSimple:
+    width = 3
+    height = 2
+
+    def calculate_area(self):
+        return self.width * self.height
+
+
+# Exemple 2 du cours : Declaration of a class with attributes and methods, including a constructor
+class Rectangle:
+    def __init__(self, length, width, color="red"):
+        self.length = length
+        self.width = width
+        self.color = color
+        
+    # Example of a method that does not return anything (void method)
+    def empty_method(self, length):
+        pass
